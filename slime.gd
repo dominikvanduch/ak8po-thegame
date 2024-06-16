0c9aff763ce95d6486866d10f4851b30f472cf85
Dominik Vanduch <75388765+dominikvanduch@users.noreply.github.com>
2024-06-16 22:53:24 +0200

    Add shooting mechanics

--- slime.gd
+++ slime.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
+var health = 3
+
 @onready var player = get_node("/root/Game/Player")
+
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 250.0
 	move_and_slide()
+	
+	
+func take_damage():
+	health -= 1
+	
+	if health == 0:
+		queue_free()
