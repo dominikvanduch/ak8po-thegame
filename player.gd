a43749ce95b6a1deacd0d9a4d27b6c3364f995a3
Dominik Vanduch <75388765+dominikvanduch@users.noreply.github.com>
2024-06-17 21:57:24 +0200

    Refactor movement

--- player.gd
+++ player.gd
@@ -1,15 +1,20 @@
 extends CharacterBody2D
 
 
+const SPEED = 400
+
+
 signal health_depleted
-
-
 var health = 100.0
 
 
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+
+
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 400
+	get_input()
 	move_and_slide()
 
 	if velocity.length() > 0.0:
@@ -17,7 +22,7 @@ func _physics_process(delta):
 	else:
 		%HappyBoo.play_idle_animation()
 
-	const DAMAGE_RATE = 50.0
+	const DAMAGE_RATE = 10.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
