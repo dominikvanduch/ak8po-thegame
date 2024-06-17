0a9c0a40485c7bc635c377d850bbea07f7fe43fc
Dominik Vanduch <75388765+dominikvanduch@users.noreply.github.com>
2024-06-17 22:40:58 +0200

    Add keyboard and mouse control for attack

--- pistol.gd
+++ pistol.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
 
-func _physics_process(delta):
+const FIRE_RATE = 0.2
+var can_fire = true
+
+
+func _physics_process(_delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range[0]
 		look_at(target_enemy.global_position)
+		
+
+func _process(delta):
+	if Input.is_action_pressed("attack") and can_fire:
+		shoot()
+		can_fire = false
+		await get_tree().create_timer(FIRE_RATE).timeout
+		can_fire = true
+
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
@@ -13,7 +26,3 @@ func shoot():
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
-
-
-func _on_timer_timeout():
-	shoot()
