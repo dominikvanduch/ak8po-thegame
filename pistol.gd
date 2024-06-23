546bf3ff613f32c6f7339bfb6ddc28e7cb92c8cd
Dominik Vanduch <75388765+dominikvanduch@users.noreply.github.com>
2024-06-23 22:55:18 +0200

    Add aim at mouse functionality

--- pistol.gd
+++ pistol.gd
@@ -6,10 +6,8 @@ var can_fire = true
 
 
 func _physics_process(_delta):
-	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range[0]
-		look_at(target_enemy.global_position)
+	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
 		
 
 func _process(delta):
