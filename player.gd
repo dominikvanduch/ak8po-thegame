3ae2f0de6728a00e86c5992c21fcc6e6f3199bf2
Dominik Vanduch <75388765+dominikvanduch@users.noreply.github.com>
2024-06-23 23:09:53 +0200

    Merge fix

--- player.gd
+++ player.gd
@@ -8,10 +8,11 @@ const DAMAGE_RATE = 10.0
 signal health_depleted
 var health = 100.0
 var last_direction = Vector2.ZERO
+var input_direction
 
 
 func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
 
 
@@ -20,17 +21,17 @@ func _physics_process(delta):
 	move_and_slide()
 
 	if velocity.length() > 0.0:
-		if direction.x > 0:
+		if input_direction.x > 0:
 			$AnimationPlayer.play("walk_right")
 			last_direction = Vector2.RIGHT
 			
-		elif direction.x < 0:
+		elif input_direction.x < 0:
 			$AnimationPlayer.play("walk_left")
 			last_direction = Vector2.LEFT
-		elif direction.y > 0:
+		elif input_direction.y > 0:
 			$AnimationPlayer.play("walk_down")
 			last_direction = Vector2.DOWN
-		elif direction.y < 0:
+		elif input_direction.y < 0:
 			$AnimationPlayer.play("walk_up")
 			last_direction = Vector2.UP
 	else:
@@ -43,7 +44,7 @@ func _physics_process(delta):
 		elif last_direction == Vector2.RIGHT:
 			$AnimationPlayer.play("idle_right")
 		
-    
+	
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
